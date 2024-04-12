595d43a36c9f5161426ef7418c4a69fb46fbea31
Max <maksym.solomyanov@gmail.com>
2024-04-12 19:29:25 +0200

    Add links, specify language, limit to 5 news, add subject, control category.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "f64765660c394c1781fbcc5fafe717ad"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
+url = f"https://newsapi.org/v2/everything?q={topic}&" \
       "from=2024-03-12&sortBy=publishedAt&apiKey=" \
-      "f64765660c394c1781fbcc5fafe717ad"
+      "f64765660c394c1781fbcc5fafe717ad&language=en"
 
 # Make a request
 request = requests.get(url)
@@ -12,13 +14,16 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-text = ""
+body = "Subject: Todays's news" + "\n"
 
 # Access the article titles and description
 for article in content["articles"][:5]:
-      text = text + article["title"] + "\n" + article["description"] + 2*"\n"
+      body = body + article["title"] \
+             + "\n" + article["description"] \
+             + "\n" + article["url"] + "\n\n"
+      
 
-text = text.encode("utf-8")
+text = body.encode("utf-8")
 send_email(text)
 
 
